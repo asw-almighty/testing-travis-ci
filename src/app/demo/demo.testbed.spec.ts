6e8b5a6c81204fb949e4a4834864c1b68059558f
sw <rj9101@jeit.co.kr>
2021-12-21 15:34:12 +0900

    .

---
+++ src/app/demo/demo.testbed.spec.ts
@@ -0,0 +1,50 @@
+import { TestBed } from '@angular/core/testing';
+import { addMatchers } from '../../testing';
+
+import { MasterService, ValueService } from './demo';
+
+export class NotProvided extends ValueService { /* example below */ }
+
+beforeEach(addMatchers);
+
+describe('demo Test!', () => {
+  let masterService: MasterService;
+  let valueServiceSpy: jasmine.SpyObj<ValueService>;
+
+  beforeEach(() => {
+    const spy = jasmine.createSpyObj('ValueService', ['getValue']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        MasterService,
+        {
+          provide: ValueService,
+          useValue: spy,
+        },
+      ],
+    });
+    masterService = TestBed.inject(MasterService);
+    valueServiceSpy = TestBed.inject(
+      ValueService
+    ) as jasmine.SpyObj<ValueService>;
+  });
+
+  it('#getValue should return stubbed value from a spy', () => {
+    const stubValue = 'stub value';
+
+    valueServiceSpy.getValue.and.returnValue(stubValue);
+    // valueServiceSpy.getValue.and.returnValue(stubValue);
+    
+    expect(masterService.getValue()).toBe(
+      stubValue,
+      'service returned stub value'
+    );
+    expect(valueServiceSpy.getValue.calls.count()).toBe(
+      1,
+      'spy method was called once'
+    );
+    expect(valueServiceSpy.getValue.calls.mostRecent().returnValue).toBe(
+      stubValue
+    );
+  });
+});
